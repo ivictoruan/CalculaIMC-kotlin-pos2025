91f9de06aba1daa3987324a828145eff4f244520
victor.pereira <victorruan135@gmail.com>
2025-03-16 16:02:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/calculaimc_kotlin_pos2025/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.calculaimc_kotlin_pos2025
+
+import android.icu.text.DecimalFormat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+    //Mesmo que "late" no Dart
+    private lateinit var etPeso : EditText
+    private lateinit var etAltura : EditText
+    private lateinit var tvImcResutado : TextView
+    private lateinit var btCalcular : Button
+    private lateinit var btLimpar : Button
+
+   private fun onIniMainActivity(){
+        etPeso = findViewById( R.id.etPeso )
+        etAltura = findViewById( R.id.etAltura )
+        tvImcResutado = findViewById( R.id.tvImcResutado )
+        btCalcular = findViewById( R.id.btCalcular )
+        btLimpar = findViewById( R.id.btLimpar )
+    }
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        onIniMainActivity()
+
+        startButtonLiteners()
+
+    }
+
+
+    private fun startButtonLiteners(){
+
+        btCalcular.setOnClickListener{
+            onCalcularBtClick()
+        }
+
+        btLimpar.setOnClickListener{
+            onLimparBtClick()
+
+        }
+    }
+    private fun onCalcularBtClick() {
+        if(etPeso.text.toString().isEmpty() || etAltura.text.toString().isEmpty() ){
+            Toast.makeText(this, "Preencha os campos!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val peso :Double = etPeso.text.toString().toDouble()
+        val altura :Double = etAltura.text.toString().toDouble()
+
+
+
+
+        val imc: Double = (peso / altura.pow(2) * 10000)
+
+        val df = DecimalFormat("0.0")
+
+        tvImcResutado.text = df.format(imc)
+
+
+    }
+    private fun onLimparBtClick() {
+        etPeso.text.clear()
+        etAltura.text.clear()
+        tvImcResutado.text = 0.0.toString()
+        etPeso.requestFocus()
+    }
+
+}
